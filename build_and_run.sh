71cfccc0f8b2bbd5a7f80db13bfcfaf7a822eeb4
decoejz <decoejz@gmail.com>
2025-01-09 20:39:27 +0000

    done

--- build_and_run.sh
+++ build_and_run.sh
@@ -5,20 +5,28 @@ GREEN="\033[0;32m" # Green color
 BLUE="\033[0;34m" # Blue color
 NC='\033[0m' # No color
 
-# cmake='/opt/homebrew/bin/cmake'
+echo "Build from zero?"
+echo -en "Res (y|N): "
+read -r BUILDING_POINT
 
-echo "Which sign scheme would you like to use?"
-echo -en "1. No signature (${BLUE}default${NC})\n2. RSA\n3. ECDSA\n\nRes: "
-read -r DES_SIGN
+if [ "$BUILDING_POINT" == "y" ]
+then
+    echo "Which sign scheme would you like to use?"
+    echo -en "1. No signature (${BLUE}default${NC})\n2. RSA\n3. ECDSA\n\nRes: "
+    read -r DES_SIGN
 
-if [ "$DES_SIGN" == "3" ]
-then
-    export SIGN_TYPE=ECDSA
-elif [ "$DES_SIGN" == "2" ]
-then
-    export SIGN_TYPE=RSA
-else
-    export SIGN_TYPE=no_sign
+    if [ "$DES_SIGN" == "3" ]
+    then
+        export SIGN_TYPE=ECDSA
+    elif [ "$DES_SIGN" == "2" ]
+    then
+        export SIGN_TYPE=RSA
+    else
+        export SIGN_TYPE=no_sign
+    fi
+
+    rm -rf build/
+
 fi
 
 # Build project
