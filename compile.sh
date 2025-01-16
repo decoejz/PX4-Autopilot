7913fc82b3e9d15c9712b39231dde60c2fd2d7df
decoejz <decoejz@gmail.com>
2025-01-16 14:11:07 +0000

    done

--- compile.sh
+++ compile.sh
@@ -7,10 +7,10 @@ NC='\033[0m' # No color
 
 echo "Build from zero?"
 echo -en "Res (y|N): "
-# read -r BUILDING_POINT
+read -r BUILDING_POINT
 
-# if [ "$BUILDING_POINT" == "y" ]
-# then
+if [ "$BUILDING_POINT" == "y" ]
+then
     # echo "Which sign scheme would you like to use?"
     # echo -en "1. No signature (${BLUE}default${NC})\n2. RSA\n3. ECDSA\n\nRes: "
     # read -r DES_SIGN
@@ -27,13 +27,13 @@ echo -en "Res (y|N): "
 
     rm -rf build/
 
-# fi
+fi
 
 # Build project
 make px4_sitl gz_x500
 
-echo "To run code execute:"
-echo
-echo "./build/px4_sitl_default/bin/px4"
+# echo "To run code execute:"
+# echo
+# echo "./build/px4_sitl_default/bin/px4"
 
 
